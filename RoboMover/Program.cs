e8a8b2714db2c277da3842b48e2d7236458ec042
CorvusDwarvus <coryjobmail@gmail.com>
2014-02-14 20:17:41 -0500

    RoboMover without Try/Catch
    This is the version that does not use Try/Catch

--- RoboMover/Program.cs
+++ RoboMover/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace RoboMover
@@ -10,6 +12,94 @@ namespace RoboMover
     {
         static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader("RoboInfo.txt");
+            string[] firstline = sr.ReadLine().Split();
+            int arraySize = Convert.ToInt32(firstline[0]);
+            int xpos = Convert.ToInt32(firstline[1]);
+            int ypos = Convert.ToInt32(firstline[2]);
+            int[,] grid = new int[arraySize, arraySize];
+            for (int i = 0; i < arraySize; i++)
+            {
+                string[] fileline = sr.ReadLine().Split();
+                for (int j = 0; j < arraySize; j++)
+                {
+                    grid[i, j] = Convert.ToInt32(fileline[j]);
+                }
+            }
+            sr.Close();
+
+            bool more = true;
+            grid[ypos, xpos] = 0;
+            while (more)
+            {
+                for (int i = 0; i < arraySize; i++)
+                {
+                    for (int j = 0; j < arraySize; j++)
+                    {
+                        Console.Write(grid[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("Which direction do you want to move?");
+                string choice = Console.ReadLine();
+                Console.Clear();
+                if (choice == "w")
+                {
+                    if (ypos - 1 >= 0 && grid[ypos - 1, xpos] == 4)
+                    {
+                        more = false;
+                        Console.WriteLine("YOU HAVE WON");
+                    }
+                    if (ypos - 1 >= 0 && grid[ypos - 1, xpos] == 1)
+                    {
+                        grid[ypos, xpos] = 1;
+                        grid[ypos - 1, xpos] = 0;
+                        ypos--;
+                    }
+                }
+                if (choice == "s")
+                {
+                    if (ypos + 1 < arraySize && grid[ypos + 1, xpos] == 4)
+                    {
+                        more = false;
+                        Console.WriteLine("YOU HAVE WON");
+                    }
+                    if (ypos + 1 < arraySize && grid[ypos + 1, xpos] == 1)
+                    {
+                        grid[ypos, xpos] = 1;
+                        grid[ypos + 1, xpos] = 0;
+                        ypos++;
+                    }
+                }
+                if (choice == "a")
+                {
+                    if (xpos - 1 >= 0 && grid[ypos, xpos - 1] == 4)
+                    {
+                        more = false;
+                        Console.WriteLine("YOU HAVE WON");
+                    }
+                    if (xpos - 1 >= 0 && grid[ypos, xpos - 1] == 1)
+                    {
+                        grid[ypos, xpos] = 1;
+                        grid[ypos, xpos - 1] = 0;
+                        xpos--;
+                    }
+                }
+                if (choice == "d")
+                {
+                    if (xpos + 1 < arraySize && grid[ypos, xpos + 1] == 4)
+                    {
+                        more = false;
+                        Console.WriteLine("YOU HAVE WON");
+                    }
+                    if (xpos + 1 < arraySize && grid[ypos, xpos + 1] == 1)
+                    {
+                        grid[ypos, xpos] = 1;
+                        grid[ypos, xpos + 1] = 0;
+                        xpos++;
+                    }
+                }
+            }
         }
     }
 }
